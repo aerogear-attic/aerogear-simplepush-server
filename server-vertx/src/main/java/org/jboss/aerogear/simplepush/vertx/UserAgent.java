9854a17cd3c5143c4d1b3d05ff3c6c11a1a82400
Daniel Bevenius <daniel.bevenius@gmail.com>
2013-05-29 11:25:06 +0200

    Got basic features working. Still need to add ack and reaper jobs.

---
+++ server-vertx/src/main/java/org/jboss/aerogear/simplepush/vertx/UserAgent.java
@@ -0,0 +1,55 @@
+/**
+ * JBoss, Home of Professional Open Source
+ * Copyright Red Hat, Inc., and individual contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.aerogear.simplepush.vertx;
+
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class UserAgent {
+    
+    private final UUID uaid;
+    private final String writeHandlerId;
+    private AtomicLong timestamp;
+
+    public UserAgent(final UUID uaid, final String writeHandlerId, final long timestamp) {
+        this.uaid = uaid;
+        this.writeHandlerId = writeHandlerId;
+        this.timestamp = new AtomicLong(timestamp);
+    }
+    
+    public UUID uaid() {
+        return uaid;
+    }
+    
+    public String writeHandlerId() {
+        return writeHandlerId;
+    }
+    
+    public long timestamp() {
+        return timestamp.get();
+    }
+    
+    public void timestamp(final long timestamp) {
+        this.timestamp.set(timestamp);
+    }
+    
+    @Override
+    public String toString() {
+        return "UserAgent[uaid=" + uaid + ", writeHandlerId=" + writeHandlerId + ", timestamp=" + timestamp() + "]";
+    }
+
+}
